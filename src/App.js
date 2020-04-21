12f9023b96586035875076b955658ae1eae6f955
iamamit-107 <ahmed.amit107@gmail.com>
2020-04-21 21:23:33 +0600

    with react and mongodb

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./page/Home/HomePage";
+import { Switch, Route } from "react-router-dom";
+import AppointmnetPage from "./page/Appoinment/AppointmnetPage";
+import Dashboard from "./page/Dashboard/Dashboard";
+import AppointmentDash from "./component/Dashboard/Appointment/Appointment";
+import Patients from "./component/Dashboard/Patients/Patients";
+import Prescription from "./component/Dashboard/Prescription/Prescription";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/appointment" component={AppointmnetPage} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route
+                    path="/appoinmentdashboard"
+                    component={AppointmentDash}
+                />
+                <Route path="/patients" component={Patients} />
+                <Route path="/prescription" component={Prescription} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import AppointmentProvider from "./Context-Api/AppointmentContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AppointmentProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AppointmentProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
